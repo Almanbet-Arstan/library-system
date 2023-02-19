4d227ecb291363768b25d3ff43d5b7900f3fcfee
Almanbet Totoev <almanbet.totoev@mail.ru>
2023-02-19 23:44:50 +0600

    first message

---
+++ src/main/java/com/example/project2Boot/repositories/BooksRepository.java
@@ -0,0 +1,12 @@
+package com.example.project2Boot.repositories;
+
+import com.example.project2Boot.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByTitleStartingWith(String title);
+}
